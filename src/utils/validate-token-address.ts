c9fb903b3cea61f085e41d03837b8e73391f2de2
Adam <zhuoshuyang@Mac.lan>
2025-01-28 20:36:53 -0500

    Nit fix on naming

--- src/utils/validate-token-address.ts
+++ src/utils/validate-token-address.ts
@@ -1,10 +1,10 @@
-export function isValidSuiAddress(address: string | undefined): boolean {
+export function isValidSuiTokenAddress(address: string | undefined): boolean {
     if (!address) {
         return false;
     }
 
-    const suiAddressRegex = /^0x[a-fA-F0-9]{64}(::[a-zA-Z0-9_]+)*$/;
-    const isValid = suiAddressRegex.test(address);
+    const suiTokenAddressRegex = /^0x[a-fA-F0-9]{64}(::[a-zA-Z0-9_]+)*$/;
+    const isValid = suiTokenAddressRegex.test(address);
 
     if (!isValid) {
         console.error(`Invalid SUI address: ${address}`);
